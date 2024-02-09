de61421381e40dcfffc0f7ec612de858e44cd5a3
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2024-02-09 09:41:59 +0100

    clear updload

---
+++ WpfBinding/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Migrations.Model;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfBinding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string ConnectionString = "Data Source=MojaBaza.db;Version=3;";
+
+        public App()
+        {
+            
+        }
+    }
+}
---
+++ WpfBinding/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+// system.data.sqlite installing
+
+namespace WpfBinding
+{
+    public partial class MainWindow : Window
+    {
+        private const string ConnectionString = "Data Source=MojaBaza.db;Version=3;";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
